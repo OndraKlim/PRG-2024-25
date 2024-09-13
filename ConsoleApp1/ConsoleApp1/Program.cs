2f3f7b1a36f237e8ad7a0d46aa7498a46d468f7b
Klimecek Ondrej <klimecek@gymvod.local>
2024-09-13 09:32:24 +0200

    Added first console project

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float a = 1.38f;
+            double b = 2.23;
+            double result = a / b;
+            bool decision = false;
+            Console.WriteLine(decision);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}

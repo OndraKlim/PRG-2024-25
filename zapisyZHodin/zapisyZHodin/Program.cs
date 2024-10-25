ecc748d3213f3542810dfcff5784f3811e20ce6f
OndraKlim <ondra.klimecek@gmail.com>
2024-10-25 08:04:15 +0200

    .

---
+++ zapisyZHodin/zapisyZHodin/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zapisyZHodin
+{
+    internal class Program
+    {
+        static void WriteNumber(int number)
+        {
+            Console.WriteLine(number);
+
+            if (number <= 1000)
+            {
+                WriteNumber(number + 1);
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+               Console.WriteLine(i);
+            }
+            WriteNumber(999);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ zapisyZHodin/zapisyZHodin/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("zapisyZHodin")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("zapisyZHodin")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5301b63b-5cfa-490a-833b-b6b43e9e4a1e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

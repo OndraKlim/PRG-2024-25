59dc23f522f8956b27a8af9b27e231434f0be4ad
OndraKlim <ondra.klimecek@gmail.com>
2024-11-08 08:04:44 +0100

    .

--- ArrayPlayground/ArrayPlayground/Program.cs
+++ ArrayPlayground/ArrayPlayground/Program.cs
@@ -89,7 +89,7 @@ namespace ArrayPlayground
             }
             if (!foundNumber)
             {
-                Console.WriteLine("cislo v poli neni");
+                Console.WriteLine("cislo v poli neni\n");
             }
             //TODO 8: Přepiš pole na úplně nové tak, že bude obsahovat 100 náhodně vygenerovaných čísel od 0 do 9.
             Random rng = new Random();
@@ -97,17 +97,33 @@ namespace ArrayPlayground
             for (int i = 0; i < myArray.Length; i++)
             {
                 myArray[i] = rng.Next(0,10);
+                Console.Write(myArray[i] + ", ");
             }
+
 
 
             //TODO 9: Spočítej kolikrát se každé číslo v poli vyskytuje a spočítané četnosti vypiš do konzole.
             int[] counts = new int[10];
-
-
+            foreach (int number in myArray)
+            {
+                counts[number]++;
+            }
+            for (int i = 0; i < counts.Length; i++)
+            {
+                Console.WriteLine("cetnost cislice " + i + " je " + counts[i]);
+            }
 
             //TODO 10: Vytvoř druhé pole, do kterého zkopíruješ prvky z prvního pole v opačném pořadí.
+            int[] reversedArray = new int[100];
 
+            for(int i = 0;i < reversedArray.Length; i++)
+            {
+                reversedArray[i] = myArray[myArray.Length-1-i];
+                Console.Write(reversedArray[i] + ", ");
+            }
 
+            //nebo reversedArray = myArray
+            //reversedArray.Reversed();
 
 
             //Zkus is dál hrát s polem dle své libosti. Můžeš třeba prohodit dva prvky, ukládat do pole prvky nějaké posloupnosti (a pak si je vyhledávat) nebo cokoliv dalšího tě napadne
---
+++ Lists/Lists/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lists
+{
+    internal class Program
+    {
+
+        static void PrintList(List<string> list)
+        {
+            
+
+        }
+        static void Main(string[] args)
+        {
+
+            List<string> myStringList = new List<string>();
+            myStringList.Add("karel");
+            myStringList.Add("xenie");
+            myStringList.Add("lojza");
+            myStringList.Add("androméda");
+            myStringList.Add("xaver");
+            myStringList.Add("cecílie");
+
+            PrintList(myStringList);
+
+            myStringList.Remove("karel");
+
+            PrintList(myStringList);
+
+            myStringList.RemoveAt(2);
+
+            PrintList(myStringList);
+
+
+
+            Dictionary <string,string> favouriteFoods = new Dictionary<string,string>();
+            favouriteFoods["Karel"] = "Buchtičky se šodo";
+            favouriteFoods["Lojza"] = "Výppečky se zelím";
+            favouriteFoods["Xaver"] = "Šišky s mákem";
+            favouriteFoods["Xénie"] = "Jitrnice s hořčicí";
+            favouriteFoods["Androméda"] = "Kabanos";
+            favouriteFoods["Cecílie"] = "Kebab";
+
+            foreach (KeyValuePair<string, string> studentAndFood in favouriteFoods)
+            {
+                string name = studentAndFood.Key;
+                string food = studentAndFood.Value;
+                Console.WriteLine("Oblíbené jídlo studenta " + name + " je " + food);
+            }
+
+            if (favouriteFoods.ContainsKey("Martin"))
+            {
+                Console.WriteLine("Martin je v seznamu");
+            }
+            else
+            {
+                Console.WriteLine("Martin není v seznamu");
+            }
+
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}

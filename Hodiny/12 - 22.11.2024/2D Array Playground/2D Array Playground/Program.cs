20f7e5aa8b537a1a0abecc25b4f2a532d1f04093
OndraKlim <ondra.klimecek@gmail.com>
2024-11-29 08:37:08 +0100

    .

--- Hodiny/12 - 22.11.2024/2D Array Playground/2D Array Playground/Program.cs
+++ Hodiny/12 - 22.11.2024/2D Array Playground/2D Array Playground/Program.cs
@@ -13,11 +13,37 @@ namespace _2D_Array_Playground
 {
     internal class Program
     {
+        static void Print2DArray(int[,] arrayToPrint)
+        {
+            for (int i = 0; i < arrayToPrint.GetLength(0); i++)
+            {
+                for (int j = 0; j < arrayToPrint.GetLength(1); j++)
+                {
+                    Console.Write(arrayToPrint[i, j] + " ");
+                }
+                Console.WriteLine(); // NEBO Console.Write("\n")
+            }
+            Console.WriteLine();
+        }
+
+        static void Reset2DArray(int[,] arrayToReset)
+        {
+            for (int i = 0; i < arrayToReset.GetLength(0); i++)
+            {
+                for (int j = 0; j < arrayToReset.GetLength(1); j++)
+                {
+                    arrayToReset[i, j] = i * 5 + j + 1;
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
             //TODO 1: Vytvoř integerové 2D pole velikosti 5 x 5, naplň ho čísly od 1 do 25 a vypiš ho do konzole (5 řádků po 5 číslech).
 
-
+            int[,] array = new int[5, 5];
+            Reset2DArray(array);
+            Print2DArray(array);
             //TODO 2: Vypiš do konzole n-tý řádek pole, kde n určuje proměnná nRow.
             int nRow = 0;
 
@@ -38,9 +64,28 @@ namespace _2D_Array_Playground
 
             //TODO 7: Otoč pořadí prvků na hlavní diagonále (z levého horního rohu do pravého dolního rohu) a vypiš celé pole do konzole po otočení.
 
+            Console.WriteLine("Prohozeni hodnot na hlavni diagonale");
+            Reset2DArray (array);
+            for (int i = 0; i < array.GetLength(1)/2; i++)
+            {
+                int secondCoord = array.GetLength(1) - 1 - i;
+                int temporary = array[i, i];
+                array[i, i] = array [secondCoord, secondCoord];
+                array[secondCoord, secondCoord] = temporary;
+            }
+            Print2DArray(array);
 
             //TODO 8: Otoč pořadí prvků na vedlejší diagonále (z pravého horního rohu do levého dolního rohu) a vypiš celé pole do konzole po otočení.
 
+            Console.WriteLine("Prohozeni hodnot na hlavni diagonale");
+            for (int i = 0; i < array.GetLength(1) / 2; i++)
+            {
+                int secondCoord = array.GetLength(1) - 1 - i;
+                int temporary = array[i, secondCoord];
+                array[i, secondCoord] = array [secondCoord, i];
+                array[secondCoord, i] = temporary;
+            }
+            Print2DArray (array);
 
             Console.ReadKey();
         }

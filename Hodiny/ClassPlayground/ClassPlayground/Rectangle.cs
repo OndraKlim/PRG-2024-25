80e3a2e787c9e2cf8acccf81225ab2cf91ef1fe3
OndraKlim <ondra.klimecek@gmail.com>
2025-01-24 07:54:57 +0100

    .

---
+++ Hodiny/ClassPlayground/ClassPlayground/Rectangle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassPlayground
+{
+    internal class Rectangle
+    {
+        private double width; private double height;
+
+        public Rectangle(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+        public double GetArea() { return width * height; }
+        public double GetWidth() { return width; }
+        public double GetHeight() { return height; }
+
+        public double GetAspectRatio() { return width / height; }
+    }
+}

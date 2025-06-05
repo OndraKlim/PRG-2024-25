07c09998ec565badafab9f6fe617ce3f21cf1f17
OndraKlim <ondra.klimecek@gmail.com>
2025-06-05 09:34:38 +0200

    vzkaz

---
+++ Vzkaz/Vzkaz/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace Vzkaz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelBackground = new System.Windows.Forms.Panel();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panelVzkaz = new System.Windows.Forms.Panel();
+            this.buttonWrite = new System.Windows.Forms.Button();
+            this.panelBackground.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelBackground
+            // 
+            this.panelBackground.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panelBackground.Controls.Add(this.buttonWrite);
+            this.panelBackground.Controls.Add(this.panelVzkaz);
+            this.panelBackground.Controls.Add(this.label1);
+            this.panelBackground.Controls.Add(this.textBoxInput);
+            this.panelBackground.Location = new System.Drawing.Point(0, 0);
+            this.panelBackground.Name = "panelBackground";
+            this.panelBackground.Size = new System.Drawing.Size(800, 450);
+            this.panelBackground.TabIndex = 0;
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.BackColor = System.Drawing.Color.White;
+            this.textBoxInput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxInput.Font = new System.Drawing.Font("MV Boli", 16F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxInput.Location = new System.Drawing.Point(0, 75);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(800, 59);
+            this.textBoxInput.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.Location = new System.Drawing.Point(20, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(379, 30);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Input what you think, max 3 words";
+            // 
+            // panelVzkaz
+            // 
+            this.panelVzkaz.Location = new System.Drawing.Point(0, 120);
+            this.panelVzkaz.Name = "panelVzkaz";
+            this.panelVzkaz.Size = new System.Drawing.Size(797, 327);
+            this.panelVzkaz.TabIndex = 2;
+            this.panelVzkaz.Paint += new System.Windows.Forms.PaintEventHandler(this.panelVzkaz_Paint);
+            // 
+            // buttonWrite
+            // 
+            this.buttonWrite.BackColor = System.Drawing.Color.DarkRed;
+            this.buttonWrite.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonWrite.ForeColor = System.Drawing.Color.Green;
+            this.buttonWrite.Location = new System.Drawing.Point(500, 10);
+            this.buttonWrite.Name = "buttonWrite";
+            this.buttonWrite.Size = new System.Drawing.Size(155, 60);
+            this.buttonWrite.TabIndex = 3;
+            this.buttonWrite.Text = "Create";
+            this.buttonWrite.UseVisualStyleBackColor = false;
+            this.buttonWrite.Click += new System.EventHandler(this.buttonWrite_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panelBackground);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panelBackground.ResumeLayout(false);
+            this.panelBackground.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelBackground;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panelVzkaz;
+        private System.Windows.Forms.Button buttonWrite;
+    }
+}
+
---
+++ Vzkaz/Vzkaz/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace Vzkaz
+{
+    public partial class Form1 : Form
+    {
+        bool play = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panelVzkaz_Paint(object sender, PaintEventArgs e)
+        {
+            if (play)
+            {
+                Graphics g = panelVzkaz.CreateGraphics();
+
+                // Nastavení fontu a barvy pro text
+                Font font = new Font("Arial", 30);
+                Brush brush = Brushes.Yellow;
+
+                // Pozice, kde se text zobrazí
+                PointF point = new PointF(20, 20);
+
+                // Kreslení textu na panelu
+                g.DrawString("Hello, Gymvod!", font, brush, point);
+            }
+        }
+
+        private void buttonWrite_Click(object sender, System.EventArgs e)
+        {
+            play = true;
+            panelVzkaz.Refresh();
+
+        }
+    }
+}
